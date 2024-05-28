ee8de54a2dd3c2e430a28b43fecf68f0579cc272
Vazquez X Brandon Eric <168706025+ericvx11@users.noreply.github.com>
2024-05-27 18:52:22 -0700

    Add files via upload

---
+++ Programa num 07.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()  
+{
+	char azar;
+	int num[10];
+	for (int i=1;1<10;i++){
+		char num[i]=azar(100)+1;
+		
+	}
+	for(int i=1;1<10;i++){
+		cout<<"los numeros aleatorios son "<<num[i]<<endl;
+	}
+	return 0;
+}
